bc8ea6b6b85c499318cf6bfac9587001d8c60f5e
Hugh Sorby <h.sorby@auckland.ac.nz>
2016-03-14 01:31:36 +1300

    Initial commit of everything.

---
+++ mapclientplugins/typeconverterstep/ui_configuredialog.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'qt/configuredialog.ui'
+#
+# Created: Sun Jul 26 23:12:27 2015
+#      by: pyside-uic 0.2.15 running on PySide 1.2.2
+#
+# WARNING! All changes made in this file will be lost!
+
+from PySide import QtCore, QtGui
+
+class Ui_ConfigureDialog(object):
+    def setupUi(self, ConfigureDialog):
+        ConfigureDialog.setObjectName("ConfigureDialog")
+        ConfigureDialog.resize(418, 303)
+        self.gridLayout = QtGui.QGridLayout(ConfigureDialog)
+        self.gridLayout.setObjectName("gridLayout")
+        self.configGroupBox = QtGui.QGroupBox(ConfigureDialog)
+        self.configGroupBox.setTitle("")
+        self.configGroupBox.setObjectName("configGroupBox")
+        self.formLayout = QtGui.QFormLayout(self.configGroupBox)
+        self.formLayout.setObjectName("formLayout")
+        self.label0 = QtGui.QLabel(self.configGroupBox)
+        self.label0.setObjectName("label0")
+        self.formLayout.setWidget(0, QtGui.QFormLayout.LabelRole, self.label0)
+        self.lineEdit0 = QtGui.QLineEdit(self.configGroupBox)
+        self.lineEdit0.setObjectName("lineEdit0")
+        self.formLayout.setWidget(0, QtGui.QFormLayout.FieldRole, self.lineEdit0)
+        self.label1 = QtGui.QLabel(self.configGroupBox)
+        self.label1.setObjectName("label1")
+        self.formLayout.setWidget(1, QtGui.QFormLayout.LabelRole, self.label1)
+        self.lineEditInputPortType = QtGui.QLineEdit(self.configGroupBox)
+        self.lineEditInputPortType.setObjectName("lineEditInputPortType")
+        self.formLayout.setWidget(1, QtGui.QFormLayout.FieldRole, self.lineEditInputPortType)
+        self.label2 = QtGui.QLabel(self.configGroupBox)
+        self.label2.setObjectName("label2")
+        self.formLayout.setWidget(2, QtGui.QFormLayout.LabelRole, self.label2)
+        self.lineEditOutputPortType = QtGui.QLineEdit(self.configGroupBox)
+        self.lineEditOutputPortType.setObjectName("lineEditOutputPortType")
+        self.formLayout.setWidget(2, QtGui.QFormLayout.FieldRole, self.lineEditOutputPortType)
+        self.gridLayout.addWidget(self.configGroupBox, 0, 0, 1, 1)
+        self.buttonBox = QtGui.QDialogButtonBox(ConfigureDialog)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName("buttonBox")
+        self.gridLayout.addWidget(self.buttonBox, 1, 0, 1, 1)
+
+        self.retranslateUi(ConfigureDialog)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("accepted()"), ConfigureDialog.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL("rejected()"), ConfigureDialog.reject)
+        QtCore.QMetaObject.connectSlotsByName(ConfigureDialog)
+
+    def retranslateUi(self, ConfigureDialog):
+        ConfigureDialog.setWindowTitle(QtGui.QApplication.translate("ConfigureDialog", "ConfigureDialog", None, QtGui.QApplication.UnicodeUTF8))
+        self.label0.setText(QtGui.QApplication.translate("ConfigureDialog", "Identifier:  ", None, QtGui.QApplication.UnicodeUTF8))
+        self.label1.setText(QtGui.QApplication.translate("ConfigureDialog", "Input Port Type:  ", None, QtGui.QApplication.UnicodeUTF8))
+        self.label2.setText(QtGui.QApplication.translate("ConfigureDialog", "Output Port Type:  ", None, QtGui.QApplication.UnicodeUTF8))
+
