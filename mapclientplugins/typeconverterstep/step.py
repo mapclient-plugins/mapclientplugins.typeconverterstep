5229be5f2921b2357de86528a6554dfcf09be11c
Hugh Sorby <h.sorby@auckland.ac.nz>
2018-12-04 22:11:46 +1300

    Fix bug with not reporting changes in port types properly.

--- mapclientplugins/typeconverterstep/step.py
+++ mapclientplugins/typeconverterstep/step.py
@@ -1,7 +1,7 @@
 
-'''
+"""
 MAP Client Plugin Step
-'''
+"""
 import json
 
 from PySide import QtGui
@@ -11,10 +11,10 @@ from mapclientplugins.typeconverterstep.configuredialog import ConfigureDialog
 
 
 class TypeConverterStep(WorkflowStepMountPoint):
-    '''
+    """
     Skeleton step which is intended to be a helpful starting point
     for new steps.
-    '''
+    """
 
     def __init__(self, location):
         super(TypeConverterStep, self).__init__('Type Converter', location)
@@ -22,56 +22,50 @@ class TypeConverterStep(WorkflowStepMountPoint):
         self._category = 'Utility'
         # Add any other initialisation code here:
         self._icon =  QtGui.QImage(':/typeconverterstep/images/utility.png')
-        # Ports:
-        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
-                      'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
-                      'http://physiomeproject.org/workflow/1.0/rdf-schema#sometype'))
-        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
-                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
-                      'http://physiomeproject.org/workflow/1.0/rdf-schema#someothertype'))
         # Port data:
         self._portDataIn = None # anothertype
         # Config:
-        self._config = {}
-        self._config['identifier'] = ''
-        self._config['Input Port Type'] = 'http://physiomeproject.org/workflow/1.0/rdf-schema#sometype'
-        self._config['Output Port Type'] = 'http://physiomeproject.org/workflow/1.0/rdf-schema#someothertype'
+        self._config = {'identifier': '',
+                        'Input Port Type': 'http://physiomeproject.org/workflow/1.0/rdf-schema#sometype',
+                        'Output Port Type': 'http://physiomeproject.org/workflow/1.0/rdf-schema#someothertype'}
 
+        # Ports:
+        self._update_ports()
 
     def execute(self):
-        '''
+        """
         Add your code here that will kick off the execution of the step.
         Make sure you call the _doneExecution() method when finished.  This method
         may be connected up to a button in a widget for example.
-        '''
+        """
         # Put your execute step code here before calling the '_doneExecution' method.
         self._doneExecution()
 
     def setPortData(self, index, dataIn):
-        '''
+        """
         Add your code here that will set the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         uses port for this step then the index can be ignored.
-        '''
+        """
         self._portDataIn = dataIn # anothertype
 
     def getPortData(self, index):
-        '''
+        """
         Add your code here that will return the appropriate objects for this step.
         The index is the index of the port in the port list.  If there is only one
         provides port for this step then the index can be ignored.
-        '''
+        """
         return self._portDataIn # atype
 
     def configure(self):
-        '''
+        """
         This function will be called when the configure icon on the step is
         clicked.  It is appropriate to display a configuration dialog at this
         time.  If the conditions for the configuration of this step are complete
         then set:
             self._configured = True
-        '''
-        dlg = ConfigureDialog()
+        """
+        dlg = ConfigureDialog(self._main_window)
         dlg.identifierOccursCount = self._identifierOccursCount
         dlg.setConfig(self._config)
         dlg.validate()
@@ -79,47 +73,50 @@ class TypeConverterStep(WorkflowStepMountPoint):
 
         if dlg.exec_():
             self._config = dlg.getConfig()
-            self._ports = []
-            self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
-                          'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
-                          self._config['Output Port Type']))
-            self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
-                          'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
-                          self._config['Input Port Type']))
+            self._update_ports()
 
         self._configured = dlg.validate()
         self._configuredObserver()
 
     def getIdentifier(self):
-        '''
+        """
         The identifier is a string that must be unique within a workflow.
-        '''
+        """
         return self._config['identifier']
 
     def setIdentifier(self, identifier):
-        '''
+        """
         The framework will set the identifier for this step when it is loaded.
-        '''
+        """
         self._config['identifier'] = identifier
 
     def serialize(self):
-        '''
+        """
         Add code to serialize this step to string.  This method should
         implement the opposite of 'deserialize'.
-        '''
+        """
         return json.dumps(self._config, default=lambda o: o.__dict__, sort_keys=True, indent=4)
 
 
     def deserialize(self, string):
-        '''
+        """
         Add code to deserialize this step from string.  This method should
         implement the opposite of 'serialize'.
-        '''
+        """
         self._config.update(json.loads(string))
 
         d = ConfigureDialog()
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
         self._configured = d.validate()
+        self._update_ports()
 
+    def _update_ports(self):
+        self._ports = []
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#provides',
+                      self._config['Output Port Type']))
+        self.addPort(('http://physiomeproject.org/workflow/1.0/rdf-schema#port',
+                      'http://physiomeproject.org/workflow/1.0/rdf-schema#uses',
+                      self._config['Input Port Type']))
 
