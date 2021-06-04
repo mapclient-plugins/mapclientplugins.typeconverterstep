d5a78c66f07c5b302a988afd6fc25b9ff7172ddb
Hugh Sorby <h.sorby@auckland.ac.nz>
2021-06-04 13:35:58 +1200

    Update step.py

--- mapclientplugins/typeconverterstep/step.py
+++ mapclientplugins/typeconverterstep/step.py
@@ -103,7 +103,7 @@ class TypeConverterStep(WorkflowStepMountPoint):
         """
         self._config.update(json.loads(string))
 
-        d = ConfigureDialog(self._main_window)
+        d = ConfigureDialog()
         d.identifierOccursCount = self._identifierOccursCount
         d.setConfig(self._config)
         self._configured = d.validate()
