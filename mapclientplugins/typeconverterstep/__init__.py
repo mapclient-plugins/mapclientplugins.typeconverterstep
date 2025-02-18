bce2bb35a7c8f2f47bb36d1293e966158496eae2
Hugh Sorby <h.sorby@auckland.ac.nz>
2025-02-18 14:28:27 +1300

    Correct __location__.

--- mapclientplugins/typeconverterstep/__init__.py
+++ mapclientplugins/typeconverterstep/__init__.py
@@ -5,7 +5,7 @@ MAP Client Plugin
 __version__ = '0.2.4'
 __author__ = 'Hugh Sorby'
 __stepname__ = 'Type Converter'
-__location__ = 'https://github.com/mapclient-plugins/typeconverterstep'
+__location__ = 'https://github.com/mapclient-plugins/mapclientplugins.typeconverterstep'
 
 # import class that derives itself from the step mountpoint.
 from mapclientplugins.typeconverterstep import step
